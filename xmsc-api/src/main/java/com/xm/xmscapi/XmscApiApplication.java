c2a40da0968ec8a443d2c5c5c6f2d4f6d2b35431
shengjianhua <shengjh@gegejia.com>
2018-10-23 17:23:46 +0800

    修正

--- xmsc-api/src/main/java/com/xm/xmscapi/XmscApiApplication.java
+++ xmsc-api/src/main/java/com/xm/xmscapi/XmscApiApplication.java
@@ -1,6 +1,8 @@
 package com.xm.xmscapi;
 
 import com.xm.xmsccommon.DefaultProfileUtil;
+import com.xm.xmsccommon.utils.LocalIpUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +11,7 @@ import org.springframework.core.env.Environment;
 //@MapperScan(basePackages = "com.xm.mapper")
 //@ComponentScan(basePackages = {"com.xm"})
 @SpringBootApplication
+@Slf4j
 public class XmscApiApplication {
 
     private static ApplicationContext context;
@@ -18,17 +21,17 @@ public class XmscApiApplication {
         DefaultProfileUtil.addDefaultProfile(app);
         context = app.run(args);
         Environment env = context.getEnvironment();
-//        log.info("\n----------------------------------------------------------\n\t" +
-//                        "Application '{}' is running! Access URLs:\n\t" +
-//                        "Local: \t\thttp://localhost:{}\n\t" +
-//                        "External: \thttp://{}:{}\n\t" +
-//                        "Active Profile(s): \t{}\n" +
-//                        "----------------------------------------------------------",
-//                env.getProperty("spring.application.name"),
-//                env.getProperty("server.port"),
-//                LocalIpUtil.getLocalHost(),
-//                env.getProperty("server.port"),
-//                env.getActiveProfiles());
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "Active Profile(s): \t{}\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                LocalIpUtil.getLocalHost(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
     }
 
     public static ApplicationContext getContext() {
